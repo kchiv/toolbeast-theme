b8125a3dcc6db2e6423dca3a1ab99218e0d1541b
Kyle Chivers <kchiv@kyles-mbp.lan>
2018-09-08 09:19:10 -0700

    updated JS for sorting

--- assets/theme.js
+++ assets/theme.js
@@ -19,3 +19,20 @@ $('.dropdown-toggle').click(function () {
 $('.dropdown-toggle').focusout(function () {
     $(this).next('.dropdown-menu').slideUp(300);
 })
+
+
+
+
+Shopify.queryParams = {};
+if (location.search.length) {
+  for (var aKeyValue, i = 0, aCouples = location.search.substr(1).split('&'); i < aCouples.length; i++) {
+    aKeyValue = aCouples[i].split('=');
+    if (aKeyValue.length > 1) {
+      Shopify.queryParams[decodeURIComponent(aKeyValue[0])] = decodeURIComponent(aKeyValue[1]);
+    }
+  }
+}
+$('#sort-by').on('change', function() {
+    Shopify.queryParams.sort_by = jQuery(this).val();
+    location.search = jQuery.param(Shopify.queryParams).replace(/\+/g, '%20');
+  });
