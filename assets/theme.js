b30405a40341e4b7cef06132be737330b727604a
Kyle Chivers <kchiv@Kyles-MacBook-Pro.local>
2018-09-23 19:07:25 -0700

    slow scrolling for jump links

--- assets/theme.js
+++ assets/theme.js
@@ -9,9 +9,7 @@ $(".closebtn").click(function(){
 });
 
 
-
 // Top nav drop down transition
-
 $('.dropdown-toggle').click(function () {
     $(this).next('.dropdown-menu').slideToggle(300);
 });
@@ -22,7 +20,6 @@ $('.dropdown-toggle').focusout(function () {
 
 
 // Collection sorting
-
 Shopify.queryParams = {};
 if (location.search.length) {
   for (var aKeyValue, i = 0, aCouples = location.search.substr(1).split('&'); i < aCouples.length; i++) {
@@ -35,4 +32,43 @@ if (location.search.length) {
 $('#sort-by').on('change', function() {
     Shopify.queryParams.sort_by = jQuery(this).val();
     location.search = jQuery.param(Shopify.queryParams).replace(/\+/g, '%20');
+  });
+
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
