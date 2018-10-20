3f0974eec40ff1299d12b945a42cdd795879fbcf
Kyle Chivers <chiverskyle@gmail.com>
2018-10-20 13:32:35 -0700

    slider functions added

--- assets/theme.js
+++ assets/theme.js
@@ -99,37 +99,18 @@ $(document).on('click', '.number-spinner button', function () {
 
 
 // PD page image slider
-$('.single-product-images').slick({
+$('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  dots: true,
-  customPaging : function(slider, i) {
-    var thumb = $(slider.$slides[i]).find('div[data-thumb]').data('thumb');
-    return `<a><img class="img-fluid" src=${thumb} /></a>`;
-  },
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true
 });
