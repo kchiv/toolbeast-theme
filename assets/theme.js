abfbe6d41d849f5900ecb4367000e36dd078d2e7
Kyle Chivers <chiverskyle@gmail.com>
2018-10-21 13:31:24 -0700

    changes variant in DOM

--- assets/theme.js
+++ assets/theme.js
@@ -94,10 +94,6 @@ $(document).on('click', '.number-spinner button', function () {
 });
 
 
-
-
-
-
 // PD page image slider
 $('.slider-for').slick({
   slidesToShow: 1,
@@ -113,4 +109,48 @@ $('.slider-nav').slick({
   dots: false,
   centerMode: true,
   focusOnSelect: true
+});
+
+
+
+function getVariantFromOptions() {
+  let variantArr = []
+  $(".product-category select").map(function(i, el) {
+    variant = {value: $(el).val(), index: $(el).data('index')};
+    variantArr.push(variant)
+  });
+  return variantArr;
+}
+
+function updateHistoryState(variant) {
+  if (!history.replaceState || !variant) {
+    return;
+  }
+
+  var newurl =
+    window.location.protocol +
+    '//' +
+    window.location.host +
+    window.location.pathname +
+    '?variant=' +
+    variant.id;
+  
+  window.history.replaceState({ path: newurl }, '', newurl);
+}
+
+$('.product-category select').on('change', function() {
+  var selectedValues = getVariantFromOptions();
+  var variants = window.product.variants;
+  
+  // Search for product variants based on what was selected in the dropdowns
+  var found = _.find(variants, function(variant) {
+    return selectedValues.every(function(values) {
+      return _.isEqual(variant[values.index], values.value);
+    });
+  });
+  updateHistoryState(found)
+  var oldMoney = found.price.toString();
+  var newMoney = "$" + oldMoney.slice(0, -2) +"."+oldMoney.slice(-2);
+  $('#variant-id').val(found.id)
+  $('.pd-pg-price').text(newMoney)
 });
